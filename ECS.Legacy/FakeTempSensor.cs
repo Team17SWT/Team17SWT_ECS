c77ffd51dc55b640af1619cbb324371efd731115
WannTheMan <jisskiss@gmail.com>
2021-02-25 12:53:43 +0100

    .

--- ECS.Legacy/FakeTempSensor.cs
+++ ECS.Legacy/FakeTempSensor.cs
@@ -4,28 +4,16 @@ namespace ECS.Legacy
 {
     public class FakeTempSensor : ITempSensor
     {
-        private int temperature;
-
-        public int Temperature
-        {
-            get
-            {
-                return this.temperature;
-            }
-            set
-            {
-                this.temperature = value;
-            }
-        }
+        public int Temperature { get; set; }
 
         public FakeTempSensor()
         {
-            temperature = 0;
+            Temperature = 0;
         }
 
         public int GetTemp()
         {
-            return temperature;
+            return Temperature;
         }
 
         public bool RunSelfTest()
